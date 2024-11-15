1577203965dd90ed2d4c7aabd9e0527634c6def3
dchrysov <j.chrisov@gmail.com>
2024-11-15 01:03:51 +0100

    functions.c: (w) It will include all the functions associated with calculating attributes and more.

---
+++ functions.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   functions.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dimitris <dimitris@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 15:04:16 by dchrysov          #+#    #+#             */
+/*   Updated: 2024/11/15 00:54:12 by dimitris         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	ft_atoi(const char *str)
+{
+	int	res;
+	int	count;
+	int	sign;
+
+	res = 0;
+	count = 0;
+	sign = 1;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	while (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign *= -1;
+		if (count > 0)
+			return (0);
+		str++;
+		count++;
+	}
+	while (*str && ft_isdigit(*str))
+	{
+		res = res * 10 + (*str - '0');
+		str++;
+	}
+	return (sign * res);
+}
+
+char	*nbr_sign(int number)
+{
+	if (number >= 0)
+		return ("positive");
+	else
+		return ("negative");
+}
+
+int	nbr_length(int value)
+{
+	int	result;
+
+	result = 0;
+	if (value == 0)
+		return (1);
+	if (value < 0)
+		value *= -1;
+	while (value > 0)
+	{
+		result++;
+		value /= 10;
+	}
+	return (result);
+}
