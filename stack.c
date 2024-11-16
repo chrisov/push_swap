0d7b9a560f45c3bf899584f21f0dfbdc9f28ec42
Dimitris Chrysovergis <dchrysov@2-G-5.42heilbronn.de>
2024-11-16 13:16:22 +0100

    stack.c: (n/w) Added doc comments.

--- stack.c
+++ stack.c
@@ -1,18 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_stack.c                                         :+:      :+:    :+:   */
+/*   stack.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dchrysov <dchrysov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 14:10:26 by dchrysov          #+#    #+#             */
-/*   Updated: 2024/11/16 12:49:43 by dchrysov         ###   ########.fr       */
+/*   Updated: 2024/11/16 13:14:38 by dchrysov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	*new_node(int value, int position_count)
+/**
+ * @brief Create a new node in the stack and init most of its attributes.
+ * 
+ * @param value The value given to the node.
+ * @param position Position of the node in the stack.
+ * 
+ * @returns The newly created node.
+ */
+t_stack	*new_node(int value, int position)
 {
 	t_stack	*node;
 
@@ -20,13 +28,24 @@ t_stack	*new_node(int value, int position_count)
 	if (!node)
 		return (NULL);
 	node->nbr = value;
-	node->init_pos = position_count;
+	node->init_pos = position;
 	node->sign = nbr_sign(value);
 	node->length = nbr_length(value);
 	node->next_nbr = NULL;
 	return (node);
 }
 
+/**
+ * @brief Initializes a stack, given an array of str.
+ * 
+ * @param s The stack to be returned.
+ * @param ptr Points to the stack and is used to traverse the array and
+ * create the nodes. 
+ * @param init_pos_count Increments every time a node is created.
+ * @param max_magn The order of magnitude of the biggest number in the array.
+ * 
+ * @returns s.
+ */
 t_stack	*stack_init(char **array)
 {
 	t_stack	*s;
@@ -51,18 +70,20 @@ t_stack	*stack_init(char **array)
 }
 
 /**
- * @param size Size of stack
+ * @brief Determines the target_position of each node in the stack.
+ * 
+ * @param magnitude Order of magnitude of the biggest number.
  * @param max_len Number digits of the biggest number in the stack.
  */
-void	target_position(t_stack *node, int digits)
+void	target_position(t_stack *node, int magnitude)
 {
 	t_stack	*ptr;
 	int		**buckets;
 
 	ptr = node;
-	buckets = buckets_alloc(ptr, digits);
+	buckets = buckets_alloc(ptr, magnitude);
 	ptr = node;
-	buckets = buckets_init(ptr, digits);
+	buckets = buckets_init(ptr, magnitude);
 }
 
 void	print_node(t_stack *head)			//<-------- PRINTF
