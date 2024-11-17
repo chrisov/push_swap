05bbfceca0cc257c58c2bf7e34ad47df5c7fc036
dchrysov <j.chrisov@gmail.com>
2024-11-17 20:24:22 +0100

    push_swap.c/.h: (n/w) Modified according to the aforementioned changes

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dimitris <dimitris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 14:10:26 by dchrysov          #+#    #+#             */
-/*   Updated: 2024/11/16 00:13:51 by dimitris         ###   ########.fr       */
+/*   Updated: 2024/11/17 20:02:31 by dimitris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,14 @@
 int	main(int argc, char **argv)
 {
 	t_stack	*a;
+	int		*int_array;
+	int		*sorted_array;
 
 	if (argc == 1)
 		return (0);
-	a = stack_init(argv);
+	int_array = atoi_array(argv, argc - 1);
+	sorted_array = buckets(int_array, argc - 1);
+	a = stack_init(int_array, sorted_array);
 	print_node(a);
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dchrysov <dchrysov@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dimitris <dimitris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 15:04:47 by dchrysov          #+#    #+#             */
-/*   Updated: 2024/11/16 12:49:51 by dchrysov         ###   ########.fr       */
+/*   Updated: 2024/11/17 20:15:41 by dimitris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <stdio.h>						// <----------------- PRINTF
 # include <stdbool.h>
 # include <unistd.h>
-#include <limits.h>
+# include <limits.h>
 
 typedef struct s_stack
 {
@@ -31,16 +31,15 @@ typedef struct s_stack
 
 int			main(int argc, char **argv);
 int			ft_atoi(const char *str);
+int			*atoi_array(char **array, int size);
 int			ft_isdigit(int c);
 int			nbr_length(int value);
-int			max_order_of_magnitude(char **array);
-int			**buckets_alloc(t_stack *s, int maxlen);
-int			**buckets_init(t_stack *s, int digits);
+int			max_order_of_magnitude(int *array);
+int			*buckets(int *array, int array_size);
 char		*nbr_sign(int number);
 char		*ft_itoa(int n);
-void		target_position(t_stack *node, int size, int max_len);
 void		print_node(t_stack *head);
-t_stack		*new_node(int value, int position);
-t_stack		*stack_init(char **array);
+t_stack		*new_node(int value, int position, int target_pos);
+t_stack		*stack_init(int *array, int *sorted_array);
 
 #endif
