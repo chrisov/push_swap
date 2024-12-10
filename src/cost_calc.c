dd9cd994a7cdbed725f3b3b4d8d6380aee19d137
Dimitris Chrysovergis <dchrysov@2-I-3.42heilbronn.de>
2024-12-10 11:38:49 +0100

    no changes

--- src/cost_calc.c
+++ src/cost_calc.c
@@ -6,7 +6,7 @@
 /*   By: dchrysov <dchrysov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 15:04:16 by dchrysov          #+#    #+#             */
-/*   Updated: 2024/12/09 14:19:33 by dchrysov         ###   ########.fr       */
+/*   Updated: 2024/12/09 15:21:43 by dchrysov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,30 @@
 
 int	cheapest_node_index(t_stack *src_node, t_stack *dest_node)
 {
+	t_stack	*current_src;
+	t_stack	*current_dest;
 	int		src_index;
 	int		dest_index;
-	int		node_index;
-	int		cost;
-	t_stack	*current_dest;
 
 	src_index = 0;
-	while (src_node)
+	current_src = src_node;
+	while (current_src)
 	{
 		current_dest = dest_node;
 		dest_index = 0;
-		while (src_node->nbr < current_dest->nbr)
+		while (current_src->nbr < current_dest->nbr)
 		{
 			dest_index++;
 			current_dest = current_dest->next_nbr;
 		}
-		if (src_index <= num_of_nodes(src_node) / 2 && dest_index <= num_of_nodes(dest_node) / 2)
-			cost = (dest_index - src_index) + 1;
+		current_src->cost = cost_calc()
 		src_index++;
-		src_node = src_node->next_nbr;
+		current_src = current_src->next_nbr;
 	}
 	return (node_index);
+}
+
+int	cost_calc()
+{
+	
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dchrysov <dchrysov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 16:50:29 by dchrysov          #+#    #+#             */
-/*   Updated: 2024/12/09 13:57:41 by dchrysov         ###   ########.fr       */
+/*   Updated: 2024/12/09 16:25:27 by dchrysov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,17 @@ int	main(int argc, char **argv)
 		push_ab(&a, &b);
 		min = stack_min(a);
 		max = stack_max(a);
-		// while (num_of_nodes(a) >= 3)
-		// {
-		// 	if (a->nbr == min || a->nbr == max)
-		// 		push(&a, &b);
-		// 	cheapest_node(a);
-		// 	push_cheapest();
-		// 	num_of_nodes(a);
-		// }
-		// sort_stack_of_three(&a);
-		// push_back();
+		while (num_of_nodes(a) >= 3)
+		{
+			if (a->nbr == min || a->nbr == max)
+				push(&a, &b);
+			target_position();
+			cheapest_node(a);
+			push_cheapest();
+			num_of_nodes(a);
+		}
+		sort_stack_of_three(&a);
+		push_back();
 		printf("a[%d]: ", num_of_nodes(a)); print_node(a);
 		printf("b[%d]: ", num_of_nodes(b)); print_node(b);
 
