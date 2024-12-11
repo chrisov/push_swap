7598d860609bb2f8f4d07abc976576fe3cd7f447
Dimitris Chrysovergis <dchrysov@2-G-5.42heilbronn.de>
2024-12-11 15:46:41 +0100

    CHECKPOINT: (W) Fixed it. Pushing back to a now :')

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dchrysov <dchrysov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 16:50:29 by dchrysov          #+#    #+#             */
-/*   Updated: 2024/12/11 15:28:55 by dchrysov         ###   ########.fr       */
+/*   Updated: 2024/12/11 15:45:48 by dchrysov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,15 @@ int	main(int argc, char **argv)
 		b = NULL;
 		push_ab(&a, &b);
 		push_ab(&a, &b);
-		while (num_of_nodes(a) >= 3)
+		while (num_of_nodes(a) > 3)
 		{
 			calculate_cost(a, b);
 			is_cheapest(a);
 			print_nodes(a, b);
 			printf("             _____________________________________________________\n\n");
 			push_cheapest(&a, &b);
-			num_of_nodes(a);
 		}
-		// sort_stack_of_three(&a);
+		sort_stack_of_three(&a);
+		print_nodes(a, b);
 	}
 }
